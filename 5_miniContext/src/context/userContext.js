4e62846a0b47281fe252fbf34946471a07cd0166
rohit <rohitsirsat855@gmail.com>
2024-02-17 20:38:43 +0530

    contex api

---
+++ 5_miniContext/src/context/userContext.js
@@ -0,0 +1,10 @@
+import React, { createContext } from "react";
+
+const UserContext = createContext();
+
+export default UserContext;
+
+// steps
+// 1 => cteate userContext from createContext() and export it
+
+// 2 => create provider .jsx
